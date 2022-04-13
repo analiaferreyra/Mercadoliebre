d337383f34f8c339e9240e2f1af8a96d6752f0c9
analiaferreyra <analiaferreyra0@gmail.com>
2022-04-13 20:18:19 -0300

    agregando procfile

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path= require ("path");
 app.use(express.static(path.resolve(__dirname, "./public")));
 
 
-app.listen(3003, () => {
-    console.log("Corriendo en http://localhost:3003");
+app.listen(process.env.PORT||3000,function() => {
+    console.log("Corriendo en http://localhost:3003")
   });
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
